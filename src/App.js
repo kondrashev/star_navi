57669816a8eea270074510cf4fbe431e37bbf21c
Pavel <pavel@Pavel.local>
2022-08-01 18:36:34 +0300

    add useContext to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // @ts-nocheck
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadModesFetchData } from "./store/get_mode/action_get";
 import endpoints from "./constants/Endpoints";
+import FormSquares from "./components/FormSquares";
+import { context } from "./constants/Context";
+
+export const ApplictationContext = createContext();
 
 const App = () => {
+  const [values, setValues] = useState(context);
   const dispatch = useDispatch();
   const listModes = useSelector((state) => state.getModesReducer.modes);
   useEffect(() => {
@@ -15,20 +20,34 @@ const App = () => {
     dispatch(loadModesFetchData(data));
   }, []);
   const toSelect = (event) => {
-    console.log(event.target.value);
+    setValues({ ...values, sizeSquares: event.target.value });
   };
   return (
-    <div className="App">
-      <select className="selectMode" onChange={toSelect}>
-        <option defaultValue="Pick">Pick mode</option>
-        {listModes.map((item) => (
-          <option key={item.name} value={item.field}>
-            {item.name}
-          </option>
-        ))}
-      </select>
-      <button className="buttonStart">START</button>
-    </div>
+    <ApplictationContext.Provider
+      value={{
+        values,
+        setValues,
+      }}
+    >
+      <div className="App">
+        <div className="containerSelect">
+          <select className="selectMode" onChange={toSelect}>
+            <option defaultValue="Pick" value={0}>
+              Pick mode
+            </option>
+            {listModes.map((item) => (
+              <option key={item.name} value={item.field}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+          <button className="buttonStart">START</button>
+        </div>
+        <div className="containerSquares">
+          <FormSquares />
+        </div>
+      </div>
+    </ApplictationContext.Provider>
   );
 };
 
---
+++ src/components/FormSquares.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormSquares = () => {
+  return <div className="formSquares"></div>;
+};
+
+export default FormSquares;
